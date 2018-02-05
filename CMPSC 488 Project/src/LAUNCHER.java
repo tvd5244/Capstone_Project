f22320be425613fdc4ed11d63c9a5e73ba23ead5
Quade <thatguy224455@gmail.com>
2018-02-05 15:31:11 -0500

    Add files via upload

--- CMPSC 488 Project/src/LAUNCHER.java
+++ CMPSC 488 Project/src/LAUNCHER.java
@@ -1,34 +1,22 @@
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
+import java.io.File;
 
 public class LAUNCHER {
 	
 	public static void main(String[] args) 
 		throws IOException {
 		HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
-		server.createContext("/", new HttpHandler() {
-			public void handle(HttpExchange req) 
-				throws IOException {
-				PrintStream out = new PrintStream(req.getResponseBody());
-				
-				String res = "<html>"
-				+ "<body>"
-				+ "<p>Hello World!</p>"
-				+ "</body>"
-				+ "</html>";
-				
-				req.sendResponseHeaders(200, res.getBytes("UTF-8").length);
-				out.print(res);
-				
-				out.close();
-			}
-		});
+		StaticPage login = new StaticPage(new File("web/login.html"));
+		
+		server.createContext("/", login);
+		server.createContext("/index", login);
+		server.createContext("/login", login);
+		server.createContext("/signup", new StaticPage(new File("web/signup.html")));
+		server.createContext("/friends", new StaticPage(new File("web/friends.html")));
+		server.createContext("/recommendations", new StaticPage(new File("web/recommendations.html")));
 		server.start();
 	}
 }
--- CMPSC 488 Project/src/StaticPage.java
+++ CMPSC 488 Project/src/StaticPage.java
@@ -1,13 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author gqj5052
- */
-public class StaticPage {
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class StaticPage 
+	implements HttpHandler {
+	File file;
 	
+	public StaticPage(File file) {
+		this.file = file;
+	}
+	
+	public void handle(HttpExchange req) 
+		throws IOException, 
+		FileNotFoundException {
+		FileInputStream in = new FileInputStream(file);
+		PrintStream out = new PrintStream(req.getResponseBody());
+		byte[] bytes = new byte[(int)file.length()];
+		
+		req.sendResponseHeaders(200, bytes.length);
+		in.read(bytes, 0, bytes.length);
+		out.write(bytes, 0, bytes.length);
+		out.close();
+		in.close();
+	}
 }
