10d7882ec548ba43b7e34da1b19413ff6ae87d11
Quade <thatguy224455@gmail.com>
2018-02-05 16:58:04 -0500

    New.
    New.

--- webserver/src/LAUNCHER.java
+++ webserver/src/LAUNCHER.java
@@ -1,8 +1,11 @@
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 import java.io.File;
+import java.util.Scanner;
 
 public class LAUNCHER {
 	
@@ -10,6 +13,23 @@ public class LAUNCHER {
 		throws IOException {
 		HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
 		StaticPage login = new StaticPage(new File("web/login.html"));
+		
+		server.createContext("/do_login", new HttpHandler() {
+			public void handle(HttpExchange req) 
+				throws IOException {
+				Scanner in = new Scanner(req.getRequestBody());
+				
+				System.out.println("received login: ");
+				
+				while (in.hasNextLine()) {
+					System.out.println(in.nextLine());
+				}
+				
+				System.out.println("done.");
+				
+				login.handle(req);
+			}
+		});
 		
 		server.createContext("/", login);
 		server.createContext("/index", login);
