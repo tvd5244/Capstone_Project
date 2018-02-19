af4a0deeae5f15666af6dc4e4704636d7cc796e8
Jonathan Shaub <shauby19@gmail.com>
2018-02-18 22:11:16 -0500

    Preliminary css for signup

---
+++ LionPals/src/web/pages/cgi-bin/do_signup.py
@@ -0,0 +1,18 @@
+import cgi
+
+print("Content-Type: text/html\r\n\r\n")
+
+print("""
+<html>
+<head>
+<style>
+table, tr, th, td {
+    border: 0.1em solid black; 
+}
+</style>
+</head>
+<body>
+<h1>Test Signup Python Script</h1>
+</body>
+</html>
+""")
