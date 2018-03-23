10c23e0ed756bdc768f04359c29b7b4f3b73c652
Brad Westhafer <35412371+bdw5204@users.noreply.github.com>
2018-03-22 23:06:15 -0400

    Update messenger_send.py

--- webserver_with_messenger/scripts/messenger_send.py
+++ webserver_with_messenger/scripts/messenger_send.py
@@ -1,1 +1,30 @@
-#Not Implemented Yet
+import cgitb; cgitb.enable()
+import cgi; fields = cgi.FieldStorage()
+import UserAccountSet
+from UserAccountPropertySet import UserAccount
+
+print("""\
+Content-Type: text/html
+\r\n
+""")
+
+recipient = fields.getvalue("recipient")
+message = fields.getvalue("message")
+
+if recipient is None or message is None: 
+    print(open("messenger_send.html", "r").read().replace("<?recipient>", recipient or "").replace("<?message>", message or ""))
+else:
+    conn = sqlite3.connect("database.db")
+    conn.executescript("""
+    create table if not exists messages (
+    ID Integer primary key autoincrement, 
+    sender String,
+    recipient String,
+    message String
+    )
+    """	)
+    conn.executescript("""
+    insert into messages (sender, recipient, message)
+    values (""" + sender """,""" + recipient """,""" + """message)
+    """ )
+    
