38c5d2ef360c2fa4baa771b5ab476dedf7edce8a
Brad Westhafer <35412371+bdw5204@users.noreply.github.com>
2018-03-22 23:35:30 -0400

    Update messenger_send.py

--- webserver_with_messenger/scripts/messenger_send.py
+++ webserver_with_messenger/scripts/messenger_send.py
@@ -1,7 +1,9 @@
 import cgitb; cgitb.enable()
 import cgi; fields = cgi.FieldStorage()
+import sqlite3
 import UserAccountSet
 from UserAccountPropertySet import UserAccount
+import Session
 
 print("""\
 Content-Type: text/html
@@ -15,16 +17,14 @@ if recipient is None or message is None:
     print(open("messenger_send.html", "r").read().replace("<?recipient>", recipient or "").replace("<?message>", message or ""))
 else:
     conn = sqlite3.connect("database.db")
-    conn.executescript("""
-    create table if not exists messages (
-    ID Integer primary key autoincrement, 
-    sender String,
-    recipient String,
-    message String
-    )
-    """	)
-    conn.executescript("""
-    insert into messages (sender, recipient, message)
-    values (""" + sender """,""" + recipient """,""" + """message)
-    """ )
+    c = conn.cursor()
+    c.execute('create table if not exists messages (ID Integer primary key autoincrement, sender String, recipient String, message String)')
+#Can't figure out how to get the sender's account id from the session.
+    sender = Session.get_account_id()
+    c.execute('insert into messages (sender, recipient, message) values (' + 
+    sender + ',' + recipient + ',' + message + ')')
+    print(open("messager_sent_message.html", "r").read()
+    .replace("<?message>", message)
+    .replace("<?recipient>", recipient)
+    .replace("<?sender>", sender))
     
