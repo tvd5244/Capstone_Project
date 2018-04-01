e0bf5450cead5c2153d2d01145fdc5f90039dcf3
Brad Westhafer <35412371+bdw5204@users.noreply.github.com>
2018-04-01 17:41:09 -0400

    Unfinished code to display messages

--- webserver_with_messenger/scripts/messenger.py
+++ webserver_with_messenger/scripts/messenger.py
@@ -1,1 +1,24 @@
-#not implemented yet
+import cgitb; cgitb.enable()
+import cgi; fields = cgi.FieldStorage()
+import sqlite3
+import UserAccountSet
+from UserAccountPropertySet import UserAccount
+from Session import Session
+
+print("""\
+Content-Type: text/html
+\r\n
+""")
+
+conn = sqlite3.connect("database.db")
+c = conn.cursor()
+session = Session.get_session()
+user_id = session.get_account_id()
+sender = c.execute('select sender, message from messages where recipient = ' + str(user_id))
+messages = ""
+for msg in c
+	messages = messages + "<
+	
+print(open("messenger_private.html", "r").read()
+.replace("<?messages>", messages)
+conn.close()
