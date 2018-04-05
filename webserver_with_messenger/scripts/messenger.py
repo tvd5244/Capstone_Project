f67cb0feca4bc7bffa3a9295ee742edfc648e973
Brad Westhafer <35412371+bdw5204@users.noreply.github.com>
2018-04-04 23:03:50 -0400

    Update messenger.py

--- webserver_with_messenger/scripts/messenger.py
+++ webserver_with_messenger/scripts/messenger.py
@@ -14,11 +14,15 @@ conn = sqlite3.connect("database.db")
 c = conn.cursor()
 session = Session.get_session()
 user_id = session.get_account_id()
-sender = c.execute('select sender, message from messages where recipient = ' + str(user_id))
+sender = c.execute('select mail from UserAccountSet where id = ' + str(user_id))
+sender = str(sender.fetchone())[2:-3]
+query = c.execute('select sender, message, timestamp from messages where recipient = "' + str(sender) + '"')
 messages = ""
-for msg in c
-	messages = messages + "<
+for msg in c:
+	messages = messages + "<tr><td>" + msg[0] + "</td>"
+	messages = messages + "<td>" + msg[1] + "</td>"
+	messages = messages + "<td>" + msg[2] + "</td></tr>"
 	
 print(open("messenger_private.html", "r").read()
-.replace("<?messages>", messages)
+.replace("<?messages>", messages))
 conn.close()
