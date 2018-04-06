e54a406b833b8e47ad3c7bc4151ce96866dadaa1
Brad Westhafer <35412371+bdw5204@users.noreply.github.com>
2018-04-06 15:46:52 -0400

    Update messenger.py

--- webserver_with_messenger/scripts/messenger.py
+++ webserver_with_messenger/scripts/messenger.py
@@ -24,7 +24,7 @@ for msg in c:
 	messages = messages + "<tr><td>" + msg[0] + "</td>"
 	messages = messages + '<td><a href="javascript:toggledisplay(msg' + str(msg[4]) + ');">' + msg[1] + '</a></td>'
 	messages = messages + "<td>" + msg[2] + "</td></tr>"
-	messages = messages + '<span id="msg' + str(msg[4]) +'" style="display: none;"><tr><td colspan="3">' + msg[3] + '</td></tr></span>'
+	messages = messages + '<tr id="msg' + str(msg[4]) +'" style="display: none;"><td colspan="3">' + msg[3] + '</td></tr>'
 	hidescript = hidescript + 'msg' + str(msg[4]) + ' = document.getElementById("msg' + str(msg[4]) +'");\r\n'
 	hidescript = hidescript + 'msg' + str(msg[4]) + '.style.display = "none";'
 	declarations = declarations + 'msg' + str(msg[4]) + '= 0'
