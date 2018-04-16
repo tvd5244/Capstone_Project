f2b377eaccb7d240c6b157b2b038eeb15d4ba478
jshaub <shauby19@gmail.com>
2018-04-16 16:06:58 -0400

    hey

--- webserver_with_messenger/scripts/html_builder.py
+++ webserver_with_messenger/scripts/html_builder.py
@@ -3,18 +3,17 @@ has_output = False
 headers = "Content-Type: text/html\r\n"
 
 
-def add_header(header): 
+def add_header(header):
 	global headers
 
-	if has_output: 
+	if has_output:
 		raise "unable to set headers, the headers have already been sent."
-	
+
 	headers = headers + header + "\r\n"
 
 
-def begin_output(): 
-	if has_output: 
+def begin_output():
+	if has_output:
 		raise "unable to set headers, the headers have already been sent."
 
 	print(headers + "\r\n")
-
---
+++ webserver_with_messenger/scripts/test.js
@@ -0,0 +1,7 @@
+function help() {
+  alert("called");
+  var body = document.getElementById("body");
+  var text = document.createTextNode("hey guys whats up");
+
+  body.appendChild(text);
+}
